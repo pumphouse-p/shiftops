d98dbd40cc12f8e2b65d8aeffb6c20bec87e539f
Devin Parrish <devin.parrish@gmail.com>
2021-06-17 17:08:09 +0000

    Fixed install script

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing the OpenShift GitOps Operator"
 oc apply -k ./gitops-operator/base
-echo "Waiting for the CRDs to exist" ; until [[ $(oc api-resources --api-group argoproj.io --no-headers | wc -l) -lt 4 ]]; do echo -n "."; sleep 3; done; echo ""
+echo "Waiting for the CRDs to exist" ; until [[ $(oc api-resources --api-group argoproj.io --no-headers | wc -l) -ge 4 ]]; do echo -n "."; sleep 3; done; echo ""
 
 echo "Installing and configuring the ArgoCD instance"
 oc apply -k ./gitops-instance/base
